a4696109d3935775f7410ee4c9280d233febe07b
Sooroosh <sooroosh.official@gmail.com>
2025-01-24 21:15:47 +0330

    first

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Controllers
+{
+
+    [ApiController]
+    [Route("api/users")]
+
+    public class UsersController:ControllerBase
+    {
+
+        //[HttpGet]
+        //public ActionResult<IEnumerable<AppUser>> GetUsers()
+        //{
+
+        //}
+    }
+}
---
+++ DatingApp/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
